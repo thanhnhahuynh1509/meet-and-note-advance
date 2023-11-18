2d51f395791acb76b2bd41aed7535e1407ef2784
Nha Huynh <nha.huynh@zien.vn>
2023-11-18 16:46:04 +0700

    [M&N-26] [BUG] ClientWidth and ClientHeight doesn't meet requirement of scroll size.

--- src/ui/main-page/Workspace.js
+++ src/ui/main-page/Workspace.js
@@ -1,14 +1,34 @@
-import { useRef } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import ScrollContainer from "../../components/utils/ScrollContainer";
 import "../../styles/ui/main-page/Workspace.css";
 
 function Workspace() {
   const workspaceRef = useRef();
+  const [scrollWidth, setScrollWidth] = useState(0);
+  const [scrollHeight, setScrollHeight] = useState(0);
+  const [resizeTrigger, setResizeTrigger] = useState(false);
+
+  const handleResize = () => {
+    setResizeTrigger((r) => !r);
+  };
+
+  useLayoutEffect(() => {
+    setScrollWidth(workspaceRef.current.offsetWidth);
+    setScrollHeight(workspaceRef.current.offsetHeight);
+  }, [resizeTrigger]);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div id="workspace" ref={workspaceRef}>
       <ScrollContainer
-        width={workspaceRef.current.clientWidth}
-        height={workspaceRef.current.clientHeight}
+        width={scrollWidth}
+        height={scrollHeight}
       ></ScrollContainer>
     </div>
   );
